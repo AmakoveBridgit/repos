547786357629b69d3e7ec3b96555d094850dc392
AmakoveBridgit <amakovebridgit@gmail.com>
2023-03-10 19:10:02 +0300

    text

---
+++ strings.js
@@ -0,0 +1,42 @@
+let x="beautiful";
+console.log("beautiful"[0]);
+console.log("beautiful"[1]);
+console.log("beautiful"[2]);
+console.log("beautiful"[3]);
+
+// question 2;
+const food="I did not have appetite today";
+console.log(`${food[10]}eat`);
+console.log(food);
+
+// question3
+
+const story="She sells sea shells at the sea shore";
+console.log()
+
+// question 4
+let word="CONfident";
+let wordA=word.toUpperCase();
+console.log(word.toUpperCase());
+
+let textA="amazing";
+let textB=textA.toLowerCase();
+console.log(textA.toLowerCase());
+
+let textC="beautiful";
+let textD=textC.toLowerCase();
+console.log(textC.toLowerCase());
+
+// question 4
+const string1="The lady went to the market with her sister"
+
+
+
+
+
+
+
+
+
+
+
